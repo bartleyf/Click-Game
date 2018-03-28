9c6c519d4ef352251f85a8c8ada47908ed871849
Frank Bartley <bartley.frank@gmail.com>
2018-03-28 15:31:41 -0500

    images not rendering, no scoring yet

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import React from 'react';
+import CardData from "./CardData.json";
+import Card from "./components/Body/Card";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/index";
+import Wrapper from "./components/Wrapper/index";
 import './App.css';
-import CardList from "./components/CardList";
 
 class App extends React.Component {
-        state = {
-        guesses: [],
-        score: 0,
-        highScore: 0
-    }
+  state = {
+    guesses: [],
+    score: 0,
+    highScore: 0,
+    CardData
+  };
 
-    validateGuess(id) {
-        if (this.state.guesses.includes(id)){
-            this.setState({guesses: this.state.guesses.push(id)});
-            this.setState({score: this.score + 1});
-        } else {
-        this.resetGame();
+  shuffleArray = array => {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
-}
+    return array;
+  };
 
-    resetGame() {
-        this.setState({guesses: []});
-        this.setState({score: 0});
+  handleOnChange = (id) => {
+      this.validateGuess(id)
+      this.shuffleArray(this.state.CardData)
+  }
+
+  validateGuess = (id) => {
+    if (this.state.guesses.includes(id)) {
+      this.setState({ guesses: this.state.guesses.push(id) });
+      this.setState({ score: this.score + 1 });
+    } else {
+      this.resetGame();
     }
+  }
+
+  resetGame() {
+    this.setState({ guesses: [] });
+    this.setState({ score: 0 });
+  }
 
   render() {
     return (
-      <CardList />
+      <div>
+      <Navbar score={this.state.score} highScore={this.state.highScore} />
+      <Wrapper>
+        {this.state.CardData.map(CardData => (
+            <span key={CardData.id}>
+                <Card
+                handleOnchange={this.handleOnChange}
+                id={CardData.id}
+                name={CardData.name}
+                image={CardData.imageSrc}
+                />
+            </span>
+        ))}
+      </Wrapper>
+      <Footer/>
+      </div>
     );
   }
 }
--- src/components/Card.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Card = props => (
-    <div className="col=sm=3">
-        <img src={props.imageSrc} alt={props.name} />
-    </div>
-)
-
-export default Card;
